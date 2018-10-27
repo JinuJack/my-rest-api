ebc9cb0ed5c7dd25a9080d7d677118dda0ed7284
Pandey <shailesh.1.pandey@nokia.com>
2018-10-27 10:04:33 +0530

    Updated

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_jwt import JWT
 from flask_restful import Api
 
-from Rest_SqlAlchemy.resources.item import Item, ItemList
-from Rest_SqlAlchemy.resources.user import UserResgister
+from resources.item import Item, ItemList
+from resources.user import UserResgister
 from security import authentication, identity
 from resources.store import Store,StoreList
 
